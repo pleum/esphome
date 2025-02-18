883b31ca18aaa24f5c0715a7be5ba12953dbc69d
pluem <pleum@users.noreply.github.com>
2025-02-18 23:18:58 +0000

    Implement empty

--- esphome/components/pms9103m/pms9103m.cpp
+++ esphome/components/pms9103m/pms9103m.cpp
@@ -3,9 +3,20 @@
 
 namespace esphome {
 namespace pms9103m {
+
 static const char *const TAG = "pms9103m";
 
 void PMS9103MComponent::setup() {}
+
+void PMS9103MComponent::loop() {}
+
+void PMS9103MComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "PMSX003:");
+  LOG_UPDATE_INTERVAL(this);
+  this->check_uart_settings(9600);
+}
+
+void PMS9103MComponent::update() {}
 
 }  // namespace pms9103m
 }  // namespace esphome
